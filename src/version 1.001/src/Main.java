3827efa06e041149ce3f7857ffb88154660e4906
Solomon Sonya <@carpenter1010>
2018-04-08 15:15:37 -0600

    Initial Push - version 1.001
    Cookie Orbiter agent created to extract Internet Explorer and Google Chrome (encyrpted cookies sqlite3 db) into a single export file and export table.
    
    Has socket connectivity to allow for remote extraction of cookies as well. Enjoy!  - Solomon Sonya

---
+++ src/version 1.001/src/Main.java
@@ -0,0 +1,29 @@
+/**  
+ * Let's do this!
+ * @author Solomon Sonya
+ *
+ */
+import java.io.*;
+import java.util.LinkedList;
+import java.util.List;
+
+import Driver.*;
+public class Main {
+
+	public static void main(String[] args) 
+	{
+		try
+		{
+			Start start = new Start(args);									
+		}
+		catch(Exception e)
+		{
+			
+		}
+		
+		
+	}
+
+	
+	
+}
